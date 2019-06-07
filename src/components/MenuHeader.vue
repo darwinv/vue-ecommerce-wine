4e6bc913bc680df5c0d3141cacccd75cc986685a
darwin <darwin.vasqz@gmail.com>
2019-06-06 21:26:35 -0500

    Initial Commit

---
+++ src/components/MenuHeader.vue
@@ -0,0 +1,36 @@
+<template>
+
+<b-container fluid class="menu-header">
+  <b-row>
+    <b-col>
+        <div>
+          <b-nav>
+            <b-nav-item>THE COLLECTION</b-nav-item>
+            <b-nav-item>ABOUT US</b-nav-item>
+          </b-nav>
+        </div>   
+      </b-col>
+  </b-row>
+</b-container>
+
+</template>
+
+<script>
+export default {
+  name: "MenuHeader"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import "../styles/globals.scss";
+</style>
+
+
+<style scoped lang="scss">
+.menu-header {
+  // margin: 40px 0 0;
+  background-color: black;
+  color: white;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    
+    <MenuHeader />
   </div>
+
+
+
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import MenuHeader from "@/components/MenuHeader.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    HelloWorld,
+    MenuHeader
   }
 };
+
+
 </script>
