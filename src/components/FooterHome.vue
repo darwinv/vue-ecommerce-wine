ba812f482e42e3b233cf848439d5219ff10e32ca
darwin <darwin.vasqz@gmail.com>
2019-06-17 23:30:21 -0500

    Footer and Backgound Green releaseed

---
+++ src/components/FooterHome.vue
@@ -0,0 +1,57 @@
+<template>
+<div>
+  <b-row class="green-bar" align-v="center">
+    <b-col>
+        <p class="align-middle">DONATE 1% FOR THE WORLD HERE</p>
+        
+      </b-col>
+  </b-row>
+
+  <b-row class="text-center footer" align-v="center">
+    <b-col>
+        <img src="@/assets/img/facebook-box.png" alt="fb">  
+        <img src="@/assets/img/instagram.png" alt="in">
+    </b-col>
+    <b-col cols="8"></b-col>
+    <b-col>
+        <a href="#"> Contact Us </a>  
+         <a href="#" class="mar20L"> FAQ</a> 
+    </b-col>
+  </b-row>
+
+</div>
+
+</template>
+
+<script>
+export default {
+  name: "FooterHome"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import "../styles/global.scss";
+
+.green-bar {
+  background-color: #133117;
+  color: white;
+  height: 40px;
+  font-size: calc(5px + 1vw);
+}
+
+
+.mar20L{
+    margin-left: 20px;
+}
+
+.footer{
+  background-color: #2E2E2E;
+  color: white;
+  height: 122px; 
+}
+
+
+</style>
+
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,8 @@
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-    
-    <MenuHeader />
+    <GreenBgHome />
+    <FooterHome />
   </div>
 
 
@@ -14,12 +14,16 @@
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
 import MenuHeader from "@/components/MenuHeader.vue";
+import FooterHome from "@/components/FooterHome.vue";
+import GreenBgHome from "@/components/GreenBgHome.vue";
 
 export default {
   name: "home",
   components: {
     HelloWorld,
-    MenuHeader
+    MenuHeader,
+    GreenBgHome,
+    FooterHome
   }
 };
 
