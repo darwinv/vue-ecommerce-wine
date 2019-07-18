8509ce62429a07a93dcffbb9438ec724391eb373
darwin <darwin.vasqz@gmail.com>
2019-07-17 22:15:06 -0500

    prueba de url base

--- src/common/api.js
+++ src/common/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const baseDomain = "https://jsonplaceholder.typicode.com";
-const baseUrl = `${baseDomain}`;
+//const baseDomain = "https://jsonplaceholder.typicode.com";
+//const url = `${baseDomain}`;
 export default axios.create({
-  baseUrl
+  baseUrl: "https://jsonplaceholder.typicode.com"
   // In case that you need a token:
   // headers : {"Authorization "}
 });
--- src/common/service_post.js
+++ src/common/service_post.js
@@ -1,6 +1,6 @@
 import api from "./api";
 
-const resource = "https://jsonplaceholder.typicode.com/posts";
+const resource = "/posts";
 export default {
   get() {
     return api.get(`${resource}`);
