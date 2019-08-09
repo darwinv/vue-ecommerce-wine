f6af89c3c83ca654aecde15f03495dcd81f9b81d
Oscar Enrique Lotero S <oscarelotero@gmail.com>
2019-08-09 01:17:37 +0000

    Change URL base to https

--- src/common/api.js
+++ src/common/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "http://localhost:8000/",
-  baseURL: "http://18.208.152.142/",
+  baseURL: "https://18.208.152.142/",
   // In case that you need a token:
   headers: getHeaders()
 });
