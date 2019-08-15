4e647a232d6a8e4ef78635513a8e74a68db5e1c7
darwin <darwin.vasqz@gmail.com>
2019-08-14 23:45:30 -0500

    integrando detalle de producto, baseurl

--- src/common/api.js
+++ src/common/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8000/",
+  baseURL: process.env.VUE_APP_API_BASE_URL,
   // baseURL: "https://18.208.152.142/",
   // In case that you need a token:
   headers: getHeaders()
